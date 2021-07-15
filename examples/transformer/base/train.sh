80cf8f0c0a059c56afb6356faeb5c230dd3a647e
denghui <denghui@sensetime.com>
2021-07-15 15:02:25 +0800

    调整训练脚本

--- examples/transformer/base/train.sh
+++ examples/transformer/base/train.sh
@@ -10,9 +10,10 @@ spring.submit arun \
     --save-dir checkpoints/transformer_wikitext103 \
     --distributed-world-size $1  --distributed-port 12343\
     --arch transformer_lm_wiki103 \
-    --max-update 286000 --lr 1.0 --t-mult 2 --lr-period-updates 270000 --lr-scheduler cosine --lr-shrink 0.75 \
-    --warmup-updates 16000 --warmup-init-lr 1e-07 --stop-min-lr 1e-09 --optimizer nag --min-lr 0.0001 --clip-norm 0.1 \
+    --max-update 150000 --lr 1.0 --lr-period-updates 60000 --lr-scheduler cosine \
+    --warmup-updates 500 --warmup-init-lr 1e-07 --stop-min-lr 1e-09 --optimizer nag --min-lr 0.0001 --clip-norm 0.1 \
     --criterion adaptive_loss --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 1 \
+    --tensorboard-logdir log \
     --sample-break-mode none --skip-invalid-size-inputs-valid-test --ddp-backend=legacy_ddp 
     --batch-size $BATCH_SIZE
     --log-interval 10 2>&1 | tee log.train"
