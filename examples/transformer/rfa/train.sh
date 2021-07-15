b33f352b910a7aaef941fbdd02d6c3e8850c93b3
qz <qinzhen@sensetime.com>
2021-07-15 17:15:52 +0800

    更新rfa训练脚本

---
+++ examples/transformer/rfa/train.sh
@@ -0,0 +1,21 @@
+BATCH_SIZE=1
+DATA_DIR=/mnt/lustre/qinzhen/nlpwork/dataset/data-bin/wikitext-103
+LR=0.0005
+MIN_LR=0.00001
+Warmup=6000
+spring.submit arun \
+    --gpu \
+    -n$1 \
+    --job-name=rfa_fairseq_wiki103\
+    --ntasks-per-node 8 --cpus-per-task 4 \
+    "fairseq-train --task language_modeling \
+    $DATA_DIR \
+    --save-dir checkpoints/transformer_wikitext103 \
+    --distributed-world-size $1  --distributed-port 12343\
+    --arch transformer_lm_rfa_wiki103 \
+    --max-update 150000 --lr $LR --t-mult 2 --lr-period-updates 270000 --lr-scheduler cosine --lr-shrink 0.75 \
+    --warmup-updates $Warmup --warmup-init-lr 1e-07 --stop-min-lr 1e-09 --optimizer nag --min-lr $MIN_LR --clip-norm 0.1 \
+    --criterion adaptive_loss --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 1 \
+    --sample-break-mode none --skip-invalid-size-inputs-valid-test --ddp-backend=legacy_ddp 
+    --batch-size $BATCH_SIZE
+    --log-interval 10 2>&1 | tee log.train"
