a45f1b6ad25656cb09c47f7df25891354eb5404f
denghui <denghui@sensetime.com>
2021-07-22 11:13:21 +0800

    add performer support module

--- fairseq/modules/__init__.py
+++ fairseq/modules/__init__.py
@@ -63,8 +63,8 @@ from .sparse_transformer_layer import SparseTransformerEncoderLayer, SparseTrans
 #from .multihead_rfa_causal_attention import MultiheadRfaCausalAttentionDebug
 #from .transformer_rfa_layer import TransformerRfaDecoderDebugLayer
 # performer
-# from .multihead_performer_attention import MultiheadPerformerAttention
-# from .performer_layer import PerformerDecoderLayer, PerformerEncoderLayer
+from .multihead_performer_attention import MultiheadPerformerAttention
+from .performer_layer import PerformerDecoderLayer, PerformerEncoderLayer
 
 __all__ = [
     "AdaptiveInput",
