c26f26010e443206d07d8fa10b8427bbe3c9f400
qz <715073608@qq.com>
2022-04-05 20:20:09 +0800

    add single head model

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -1316,6 +1316,10 @@ def roberta_prenorm_architecture(args):
 def roberta_base_architecture(args):
     base_architecture(args)
 
+@register_model_architecture("roberta", "roberta_base_single_head")
+def roberta_base_architecture(args):
+    base_architecture(args)
+    args.encoder_attention_heads = 1
 
 @register_model_architecture("roberta", "roberta_large")
 def roberta_large_architecture(args):
@@ -2466,6 +2470,23 @@ def roberta_cosformer_architecture(args):
     args.causal = False
     args.has_out = False
     args.encoder_attention_heads = 1
+    args.encoder_normalize_before = True
+    args.use_gelu = True
+    args.mem_use_gelu = True
+    args.has_out = True
+    ## add
+    args.act_fun = "elu"
+    args.norm_type = "layernorm"
+    args.out_use_act = False
+
+@register_model_architecture("roberta_mem", "roberta_mem_hasout_elu_out_no_act_6head")
+def roberta_cosformer_architecture(args):
+    base_architecture(args)
+    args.use_relu = getattr(args, "use_relu", True)
+    args.max_l = getattr(args, "max_l", 512)
+    args.causal = False
+    args.has_out = False
+    args.encoder_attention_heads = 6
     args.encoder_normalize_before = True
     args.use_gelu = True
     args.mem_use_gelu = True
