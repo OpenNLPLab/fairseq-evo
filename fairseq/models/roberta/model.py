c78fe8b791221da0c1d03ddf4dc196c1b10fec44
qz <715073608@qq.com>
2022-05-20 14:41:41 +0800

    add glu speed test

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -4181,6 +4181,22 @@ def roberta_base_architecture(args):
     args.use_glu = True
     args.glu_act = "swish"
 
+# speed test
+@register_model_architecture("roberta_norm_attention", "roberta_rms_layer_standard")
+def roberta_base_architecture(args):
+    base_architecture(args)
+    ### add
+    args.linear_act_fun = "elu"
+    args.local_act_fun = "relu"
+    args.max_l = getattr(args, "max_l", 512)
+    args.has_out = True
+    args.encoder_attention_heads = 12
+    args.encoder_use_orpe = False
+    args.group_type = "chunk"
+    args.encoder_chunk_size = 64
+    args.attention_types = [2 for _ in range(args.encoder_layers // 2)] + [1 for _ in range(args.encoder_layers // 2)]
+    args.local_norm_type = "simplermsnorm"
+
 @register_model_architecture("roberta_norm_attention", "roberta_glu_all_layernorm")
 def roberta_base_architecture(args):
     base_architecture(args)
