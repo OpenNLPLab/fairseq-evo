fb8a0ab278bdc949674645795f8701941ff7ddac
Doraemonzzz <715073608@qq.com>
2022-06-21 11:24:54 +0800

    add pe abl

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -4519,6 +4519,28 @@ def roberta_base_architecture(args):
 #### layer norm rms
 
 #### GLU + ORPE
+@register_model_architecture("roberta_norm_attention", "roberta_glu_all_rms_layer_ln_rms_no_orpe")
+def roberta_base_architecture(args):
+    base_architecture(args)
+    ### add
+    args.linear_act_fun = "elu"
+    args.local_act_fun = "relu"
+    args.max_l = getattr(args, "max_l", 512)
+    args.has_out = True
+    args.encoder_attention_heads = 12
+    args.encoder_use_orpe = False
+    args.group_type = "chunk"
+    args.encoder_chunk_size = 64
+    args.encoder_attention_types = [2 for _ in range(args.encoder_layers // 2)] + [1 for _ in range(args.encoder_layers // 2)]
+    args.local_norm_type = "simplermsnorm"
+    args.norm_type = "simplermsnorm"
+    ### glu
+    args.use_glu = True
+    args.glu_act = "swish"
+    args.attn_type = "simplermsnorm"
+    ###### orpe
+    args.encoder_use_orpe = False
+
 @register_model_architecture("roberta_norm_attention", "roberta_glu_all_rms_layer_ln_rms_orpe_1d3")
 def roberta_base_architecture(args):
     base_architecture(args)
@@ -6496,6 +6518,30 @@ def roberta_base_architecture(args):
 # mix 并联
 
 ##### local global new version
+@register_model_architecture("roberta_norm_attention", "roberta_glu_all_rms_layer_ln_rms_no_orpe_softmax_1+elu")
+def roberta_base_architecture(args):
+    base_architecture(args)
+    ### add
+    args.linear_act_fun = "1+elu"
+    args.local_act_fun = "relu"
+    args.max_l = getattr(args, "max_l", 512)
+    args.has_out = True
+    args.encoder_attention_heads = 12
+    args.encoder_use_orpe = False
+    args.group_type = "chunk"
+    args.encoder_chunk_size = 64
+    args.encoder_attention_types = [2 for _ in range(args.encoder_layers // 2)] + [1 for _ in range(args.encoder_layers // 2)]
+    args.local_norm_type = "simplermsnorm"
+    args.norm_type = "simplermsnorm"
+    ### glu
+    args.use_glu = True
+    args.glu_act = "swish"
+    args.attn_type = "simplermsnorm"
+    ###### orpe
+    args.encoder_use_orpe = False
+    ###### softmax
+    args.use_softmax = True
+
 @register_model_architecture("roberta_norm_attention", "roberta_glu_all_rms_layer_ln_rms_orpe_1d3_softmax_1+elu")
 def roberta_base_architecture(args):
     base_architecture(args)
