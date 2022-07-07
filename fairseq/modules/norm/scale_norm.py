8e005cf8c2d840a9e0132a38ce133b0466495ae7
Doraemonzzz <715073608@qq.com>
2022-07-08 00:07:03 +0800

    fix scale norm bug

--- fairseq/modules/norm/scale_norm.py
+++ fairseq/modules/norm/scale_norm.py
@@ -3,8 +3,9 @@ import torch
 from torch import nn
 
 class ScaleNorm(nn.Module):
-    def __init__(self, eps=1e-5):
+    def __init__(self, d, eps=1e-5):
         super().__init__()
+        self.d = d
         self.eps = eps
         self.scala = nn.Parameter(torch.ones(1))
 
